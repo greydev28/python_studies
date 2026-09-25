8484384b3ef4eeeb447750d8265b1a16c85d45c1
greydev <obebenny28@gmail.com>
2026-09-25 16:36:29 +0100

    Update repository link and files

--- Part 03 — 🛠️ Functions/solutions/b4.py
+++ Part 03 — 🛠️ Functions/solutions/b4.py
@@ -1,9 +1,12 @@
 #football Academy Manager
 
-players = []
+players = [
+]
+
+
 
 def cleaned(name):
-  return name.strip().lower()
+  return name.strip().title()
 
 def add_player():
     username = input("enter fullname? ")
@@ -13,7 +16,7 @@ def add_player():
     while True:
 
       enter_position = int(input("enter prefered position(10,17,7,8,9,12)  "))
-      
+
       if enter_position not in  unique_position:
        return "invalid position"
     
@@ -25,9 +28,10 @@ def add_player():
       age = int(input("enter your age...  "))
     except ValueError:
         return "age not right format"
-
+    
     if 12 < age > 30:
-      return "age not accepted in academy"
+      print("age not accepted in academy")
+      return
     try:
       number_goals = int(input("enter number of goals? "))
       number_assists = int(input("number of assists made? "))
@@ -36,8 +40,6 @@ def add_player():
 
     if number_assists < 0:
       return False
-   
-    
     players.append({
         "name": cleaned_username,
         "age":age,
@@ -61,13 +63,71 @@ def find_player(name):
       player_found.append(player)
   return player_found
 
-def show_players():
+def show_players(players):
   for player in players:
     print(f'Name: {player["name"]}\nAge: {player["age"]}\nPosition: {player["position"]}\nGoal: {player["goals"]}\nAssists: {player["assists"]}\n')
 
-def count_players():
+def count_players(players):
   return len(players)
 
+def topscorer(players):
+  topscorer_player_container = []
+  topscorer_player = players[0]["goals"]
+  for player in players:
+    if topscorer_player < player["goals"]:
+      topscorer_player = player["goals"]
+  for player in players:
+       if topscorer_player == player["assists"]:
+          topscorer_player_container.append(player['name'])
+  return topscorer_player_container
+
+def most_assists(players):
+  most_assist_container = []
+  most_assists_player = players[0]["assists"]
+  for player in players:
+    if most_assists_player < player["assists"]:
+      most_assists_player = player["assists"]
+  for player in players:
+      if most_assists_player == player["assists"]:
+         most_assist_container.append(player['name'])
+  return most_assist_container
+
+
+def youngest_player(players):
+  youngest_player_container = []
+  youngest_player = players[0]["age"]
+  for player in players:
+    if player["age"] < youngest_player:
+      youngest_player = player["age"]
+  for player in players:
+    if player["age"] == youngest_player:
+      youngest_player_container.append(player['name'])
+  return youngest_player_container
+
+def average_age(players):
+  total = 0
+  total_player = len(players)
+  for player in players:
+    total += player["age"]
+  average = total / total_player
+  return average
+
+def average_goals():
+  total = 0
+  active_goals = 0
+
+  if active_goals == 0:
+    return 0.0
+  for player in players:
+    if player['goals'] > 0:
+      active_goals += 1
+      total += player["goals"]
+  average_goals_scored = total / active_goals
+  return average_goals_scored
+  
+
+def academy_summary(players):
+   print(f"Players: {count_players(players)}\nAverage Age: {average_age(players)}\nTop Scorer: {topscorer(players)}\nMost Assists: {most_assists(players)}\nYoungest: {youngest_player(players)}")
 def menu():
   print("======student academy======")
   print("1...add player")
@@ -75,14 +135,21 @@ def menu():
   print("3.... find player")
   print("4.... show players")
   print("5.... count players")
-  print("6.... exit")   
-
+  print("6....  topscorer")
+  print("7.... most assists")
+  print("8.... youngest player")
+  print("9...  Average age")
+  print("10... avarage goals")
+  print("11.... academy_summmary")
+  print("12.... exit")   
+  
 
 def main():
   while True:
     menu()
     choice = input("make a choice ")
-    if choice not in {"1","2","3","4","5","6"}:
+    unique_number = {"1","2","3","4","5","6","7","8","9","10","11"}
+    if choice not in unique_number :
       return f'{choice}- not available'
     if choice == "1":
       add_player() 
@@ -91,17 +158,31 @@ def main():
       clean_name = cleaned(enter_player_name)
       remove_player(clean_name)
     elif choice == "3":
-       enter_player_name = input("search for player with name.... ")
-       clean_name = cleaned(enter_player_name)
-       found_player = find_player(clean_name)
-       print(found_player)
+      enter_player_name = input("search for player with name.... ")
+      clean_name = cleaned(enter_player_name)
+      found_player = find_player(clean_name)
+      print(found_player)
     elif choice == "4":
-       show_players()
-    if choice == "5":
-      return count_players()
-    if choice == "6":
-      print("thanks for watching out!!!")
+      show_players(players)
+    elif choice == "5":
+      count_players(players)
+    elif choice == "6":
+      topscorer(players)
+    elif choice == "7":
+      most_assists(players)
+    elif choice == "8":
+      youngest_player(players)
+    elif choice == "9":
+      average_age(players)
+    elif choice == "10":
+      average_goals(players)
+    elif choice == "11":
+      academy_summary(players)
+    elif choice == "12":
+      print("thanks for checking us out")
       break
+
+      
 
 main()
     
--- Part 03 — 🛠️ Functions/solutions/test.py
+++ Part 03 — 🛠️ Functions/solutions/test.py
@@ -1,6 +1,49 @@
-def is_palindrome(s: str) -> str:
-  rvsd_str = ''
-  for char in reversed(s):
-    rvsd_str += char
-  return s == rvsd_str
-print(is_palindrome('madam'))
+players = [
+    {"name": "Daniel", "age": 22, "goals": 15, "assists": 2},
+    {"name": "Ibrahim", "age": 23, "goals": 5, "assists": 11},
+    {"name": "James", "age": 17, "goals": 2, "assists": 4},
+    
+]
+
+'''def sort_players(players,keyfunction):
+  return keyfunction(players)
+
+def goals_key(players):
+    players.sort(key= lambda player:player["goals"])
+    return players
+
+def assists_key(players):
+   return sorted(players,key= lambda player:player["assists"] ,reverse=True)
+
+def youngest_key(players):
+   return sorted(players,key= lambda player:player["age"])
+
+print(sort_players(players,goals_key))
+print(sort_players(players,assists_key))
+print(sort_players(players,youngest_key))'''
+
+goals_key = sorted(players,key= lambda player:player["goals"])
+print(goals_key)
+assists_key = sorted(players,key= lambda player:player["assists"] ,reverse=True)
+print(assists_key)  
+youngest_key = sorted(players,key= lambda player:player["age"]) 
+print(youngest_key)
+
+
+#debugging 
+def add_player(player,squad=None):
+  if squad is None:
+    squad = []
+  squad.append(player)
+  return squad
+print(add_player({"name": "John", "age": 20, "goals": 10, "assists": 5},players))
+
+def multiply(x):
+    return x * 2
+
+def calculate(func, value):
+    return func(value)
+
+print(
+    calculate(multiply, 8)
+)
